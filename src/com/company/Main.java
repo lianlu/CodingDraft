41c8ca1924c71f91ef32f5e61b5ca3f28c67b6cc
lian lu <llatsinghua@gmail.com>
2018-12-30 21:18:35 -0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,254 @@ import java.security.spec.DSAGenParameterSpec;
 import java.util.*;
 
 
+
+
 public class Main {
     public static void main(String[] args) {
-        //double s = new Solution().openLock(new String[]{"8887","8889","8878","8898","8788","8988","7888","9888"}, "8888");
-        //List<List<Interval>> input = new ArrayList<>();
-        // List<Interval>
-        // System.out.println(s);
-        List<String> input = new ArrayList<String>();
-        input.add("XYD");
-        input.add("YZE");
-        input.add("DEA");
-        // boolean s = new Solution().pyramidTransition("XYZ", input);
-        System.out.println(new Solution().minSwapsCouples(new int[]{0, 2, 1, 3}));
+        //System.out.println(s);
     }
 }
 
 class Solution {
+
+
+
+    Map<TreeNode, Integer> noCover = new HashMap<>();
+    Map<TreeNode, Integer> needsCover = new HashMap<>();
+    public int minCameraCover(TreeNode root) {
+        return needsToCoverRoot(root);
+    }
+    private int needsToCoverRoot(TreeNode root){
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (needsCover.containsKey(root)) return needsCover.get(root);
+
+        int op1 = 1 + noCoverRoot(root.left) + noCoverRoot(root.right);
+        if (root.left != null) {
+            int op2 = 1 + noCoverRoot(root.left.left) + noCoverRoot(root.left.right) + needsToCoverRoot(root.right);
+            op1 = Math.min(op1, op2);
+        }
+
+        if (root.right != null) {
+            int op2 = 1 + noCoverRoot(root.right.left) + noCoverRoot(root.right.right) + needsToCoverRoot(root.left);
+            op1 = Math.min(op1, op2);
+        }
+
+        needsCover.put(root, op1);
+        return op1;
+    }
+
+    private int noCoverRoot(TreeNode root){
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 0;
+        if (noCover.containsKey(root)) return noCover.get(root);
+
+        int op1 = needsToCoverRoot(root.left) + needsToCoverRoot(root.right);
+        int op2 = 1 + noCoverRoot(root.left) + noCoverRoot(root.right);
+
+        int res = Math.min(op1, op2);
+        noCover.put(root, res);
+        return res;
+    }
+
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> originalList = new HashSet<>();
+        for (String w : wordlist)
+            originalList.add(w);
+
+        Map<String, String> caseMatches = new HashMap<>();
+        for (String w : wordlist) {
+            if (!caseMatches.containsKey(w.toLowerCase())) {
+                caseMatches.put(w.toLowerCase(), w);
+            }
+        }
+
+        Map<String, String> vowelMap = new HashMap<>();
+        for (String w : wordlist) {
+            String regexWord = convertWord(w.toLowerCase());
+            if (!vowelMap.containsKey(regexWord)) {
+                vowelMap.put(regexWord.toLowerCase(), w);
+            }
+        }
+
+        String []res = new String[queries.length];
+        for (int i = 0; i < queries.length; i++){
+            String curWord = queries[i];
+            if (originalList.contains(curWord)) res[i] = curWord;
+            else if (caseMatches.containsKey(curWord.toLowerCase())) {
+                res[i] = caseMatches.get(curWord.toLowerCase());
+            }
+            else {
+                String cw = convertWord(curWord.toLowerCase());
+                if (vowelMap.containsKey(cw)) {
+                    res[i] = vowelMap.get(cw);
+                }
+                else {
+                    res[i] = "";
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private String convertWord(String w) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < w.length(); i++) {
+            char c = w.charAt(i);
+            if (c == 'a' || c == 'e' || c== 'i' || c == 'o' || c == 'u') {
+                sb.append("*");
+            }
+            else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public int[] numsSameConsecDiff(int N, int K) {
+        if (N == 1) {
+            return new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        }
+        else {
+            int[]lastResult = numsSameConsecDiff(N - 1, K);
+            List<Integer> res = new LinkedList<>();
+            for (int v : lastResult) {
+                Integer nextValue = findNextValue(v, K);
+                if (nextValue != null) {
+                    res.add(nextValue);
+                }
+
+                if (K != 0) {
+                    Integer prevValue = findPrevValue(v, K);
+                    if (prevValue != null && prevValue != nextValue)
+                        res.add(prevValue);
+
+                }
+            }
+
+            int []r = new int[res.size()];
+            int i = 0;
+            for (int x : res)
+                r[i++] = x;
+
+            return r;
+        }
+    }
+
+    private Integer findPrevValue(int v, int K) {
+        if (v == 0) return null;
+        int lastDigit = v%10;
+        if (lastDigit - K >= 0 ) {
+            return v * 10 + (lastDigit - K);
+        }
+
+        return null;
+    }
+
+    private Integer findNextValue(int v, int K) {
+        if (v == 0) return null;
+        int lastDigit = v%10;
+        if (lastDigit + K < 10 ) {
+            return v * 10 + (lastDigit + K);
+        }
+
+        return null;
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (root.left != null && root.left.val != root.val) return false;
+        if (root.right != null && root.right.val != root.val) return false;
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+
+    public int maxChunksToSorted(int[] arr) {
+        int[]arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            arr2[i] = arr[i];
+
+        Arrays.sort(arr2);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < arr2.length; i++) {
+            map.put(arr2[i], map.getOrDefault(arr2[i], 0) + 1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) - 1);
+
+            if (map.get(arr[i]) == 0) map.remove(arr[i]);
+            if (map.containsKey(arr2[i]) && map.get(arr2[i]) == 0) map.remove(arr2[i]);
+
+            if (map.size() == 0) {
+                count++;
+                map.clear();
+            }
+        }
+
+        return count;
+    }
+
+    public double minmaxGasDist(int[] stations, int K) {
+        double lo = 0;
+        Arrays.sort(stations);
+        double hi = stations[stations.length - 1];
+        double eps = 0.000001;
+        while(hi - lo > eps) {
+            double mid = (lo + hi)/2;
+            if (canFill(stations, K, mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid;
+            }
+        }
+
+        return lo;
+    }
+
+    private boolean canFill(int[] stations, int k, double mid) {
+        for (int i = 1; i < stations.length; i++) {
+            double diff = stations[i] - stations[i - 1];
+            int wanted = (int)(diff/mid);
+            k -= wanted;
+            if (k < 0) return false;
+        }
+
+        return true;
+    }
+
+    public String reorganizeString(String S) {
+        int[]count = new int[26];
+        int[]validPos = new int[26];
+        for (char c : S.toCharArray()) {
+            count[c-'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            int candidate = findCandidate(count, validPos, i);
+            if (candidate == -1) return "";
+            sb.append((char)('a' + candidate));
+        }
+
+        return sb.toString();
+    }
+
+    private int findCandidate(int[] count, int[] validPos, int curIndex) {
+        int maxIndex = -1;
+        for (int i = 0; i < count.length; i++) {
+            if (validPos[i] >= curIndex) {
+                if (maxIndex == -1 || count[maxIndex] < count[i]) {
+                    maxIndex = i;
+                }
+            }
+        }
+
+        return maxIndex;
+    }
+
     public int minSwapsCouples(int[] row) {
         Map<Integer, Integer> map = new HashMap<>(); // group to index
         DJSet djSet = new DJSet(row.length);
@@ -135,9 +367,6 @@ class Solution {
 
         return -1;
     }
-
-
-
 }
 
 class Interval {
@@ -155,3 +384,9 @@ class Interval {
     }
 }
 
+class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
